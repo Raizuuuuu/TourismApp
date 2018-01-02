cd9b0b2456628e92970a2d6ad826b899e9557b73
Neo Lok Jun <NEOLOKJUN.17@ichat.sp.edu.sg>
2018-01-03 04:04:35 +0800

    fkl

--- Assign1/findToursForm.Designer.cs
+++ Assign1/findToursForm.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             this.homeButton = new System.Windows.Forms.Button();
-            this.searchButton = new System.Windows.Forms.Button();
-            this.searchBox = new System.Windows.Forms.TextBox();
             this.seeCartButton = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -51,28 +49,11 @@
             this.homeButton.UseVisualStyleBackColor = true;
             this.homeButton.Click += new System.EventHandler(this.homeButton_Click);
             // 
-            // searchButton
-            // 
-            this.searchButton.Location = new System.Drawing.Point(578, 48);
-            this.searchButton.Name = "searchButton";
-            this.searchButton.Size = new System.Drawing.Size(80, 37);
-            this.searchButton.TabIndex = 1;
-            this.searchButton.Text = "Search";
-            this.searchButton.UseVisualStyleBackColor = true;
-            // 
-            // searchBox
-            // 
-            this.searchBox.Location = new System.Drawing.Point(259, 48);
-            this.searchBox.Name = "searchBox";
-            this.searchBox.Size = new System.Drawing.Size(270, 26);
-            this.searchBox.TabIndex = 2;
-            this.searchBox.Text = "Search Tour...";
-            // 
             // seeCartButton
             // 
             this.seeCartButton.Location = new System.Drawing.Point(24, 116);
             this.seeCartButton.Name = "seeCartButton";
-            this.seeCartButton.Size = new System.Drawing.Size(80, 38);
+            this.seeCartButton.Size = new System.Drawing.Size(92, 38);
             this.seeCartButton.TabIndex = 3;
             this.seeCartButton.Text = "See Cart";
             this.seeCartButton.UseVisualStyleBackColor = true;
@@ -87,10 +68,10 @@
             this.Country,
             this.Duration,
             this.Itinenary});
-            this.dataGridView1.Location = new System.Drawing.Point(156, 145);
+            this.dataGridView1.Location = new System.Drawing.Point(142, 116);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(547, 333);
+            this.dataGridView1.Size = new System.Drawing.Size(815, 333);
             this.dataGridView1.TabIndex = 6;
             // 
             // Title
@@ -122,25 +103,20 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(715, 543);
+            this.ClientSize = new System.Drawing.Size(1031, 472);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.seeCartButton);
-            this.Controls.Add(this.searchBox);
-            this.Controls.Add(this.searchButton);
             this.Controls.Add(this.homeButton);
             this.Name = "findToursForm";
             this.Text = "Find Tours";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button homeButton;
-        private System.Windows.Forms.Button searchButton;
-        private System.Windows.Forms.TextBox searchBox;
         private System.Windows.Forms.Button seeCartButton;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Title;
