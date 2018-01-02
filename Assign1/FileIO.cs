6be9a736b4ea52ac49c031a7c47ead3155b0dd87
YONGZHENGLEOW.17 <YONGZHENGLEOW.17@ichat.sp.edu.sg>
2018-01-03 03:28:20 +0800

    create basic class for file interaction

---
+++ Assign1/FileIO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign1
+{
+    class FileIO
+    {
+        //class for file read/write.
+        //If functionality is already present in other classes, feel free to delete
+        public static 
+    }
+}
